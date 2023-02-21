cb18fb9bac657aacce38ce7c04447fd8acfa1fcd
FranGanuza99 <gr18015@ues.edu.sv>
2023-02-21 11:02:44 -0600

    Update guide.php

--- routes/guide/guide.php
+++ routes/guide/guide.php
@@ -3,15 +3,12 @@
 use App\Http\Controllers\API\GuideController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('guide', [GuideController::class, 'index'])->middleware('permissions:can_manage_sales');
+//Route::get('guide', [GuideController::class, 'index'])->middleware('permissions:can_manage_sales');
 Route::post('guides', [GuideController::class, 'getAllGuides']);
 Route::get('guide/{id}', [GuideController::class, 'show']);
-Route::post('guide/store', [GuideController::class, 'store'])->middleware('permissions:can_manage_sales');
-Route::post('guide/{id}', [GuideController::class, 'update'])->middleware('permissions:can_manage_sales');
-Route::post('guide/delete/{id}', [GuideController::class, 'destroy']);
+Route::post('guide/store', [GuideController::class, 'store'])->middleware('permissions:can_manage_guides');
+Route::post('guide/{id}', [GuideController::class, 'update'])->middleware('permissions:can_manage_guides');
+Route::post('guide/delete/{id}', [GuideController::class, 'destroy'])->middleware('permissions:can_manage_guides');;
 
-
-/*Route::get('/get-departamentos', [DepartamentoController::class, 'getDepartamentos']);
-Route::get('/get-municipios-departamentoId/{department_id}', [MunicipioController::class, 'getMunicipiosByDepId']);*/
-//Route::get('guide', [GuideController::class, 'guide'])->middleware('permissions:can_manage_guides');
+Route::get('guide', [GuideController::class, 'guide'])->middleware('permissions:can_manage_guides');
 //Route::get('guides', [GuideController::class, 'index'])->middleware('permissions:can_manage_guides');
